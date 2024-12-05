b04335e449c4a1e3f34f9d46994b7646389961b4
USER <jhonatangadi@gmail.com>
2024-12-05 10:59:16 +0200

    first ever app thingy!

--- app/src/main/java/com/example/sleepapp1/SleepSchoola.java
+++ app/src/main/java/com/example/sleepapp1/SleepSchoola.java
@@ -10,10 +10,15 @@ import android.widget.ImageView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import kotlinx.coroutines.channels.ChannelResult;
+
 public class SleepSchoola extends AppCompatActivity implements View.OnClickListener {
 
-    Button sleepActiclebtn1;
+    Button SleepArticlebtn1;
     Button SleepArticlebtn2;
+    Button SleepArticlebtn3;
+    Button SleepArticlebtn4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +26,68 @@ public class SleepSchoola extends AppCompatActivity implements View.OnClickListe
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sleep_schoola);
 
-        sleepActiclebtn1 = findViewById(R.id.sleeparticlebtn);
-        sleepActiclebtn1.setOnClickListener(this);
+        SleepArticlebtn1 = findViewById(R.id.sleeparticlebtn);
+        SleepArticlebtn1.setOnClickListener(this);
 
         SleepArticlebtn2 = findViewById(R.id.Sleeptips);
         SleepArticlebtn2.setOnClickListener(this);
+
+        SleepArticlebtn3 = findViewById(R.id.BluelightArticle);
+        SleepArticlebtn3.setOnClickListener(this);
+
+        SleepArticlebtn4 = findViewById(R.id.sleepimportant);
+        SleepArticlebtn4.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
-        if (view == sleepActiclebtn1) {
+        if (view == SleepArticlebtn1) {
             Showdialog();
         }
 
         if (view == SleepArticlebtn2) {
             Showdialog2();
         }
+
+        if (view == SleepArticlebtn3){
+            Showdialog3();
+        }
+        
+        if (view == SleepArticlebtn4){
+            Showdialog4();
+        }
     }
+    
+    private void Showdialog4(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.whysleepimportant);
+        
+        ImageView closedialog = dialog.findViewById(R.id.Closedialogbtn);
+        closedialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {dialog.dismiss();}
+        });
+        
+
+        dialog.show();
+    }
+    
+    
+    private void Showdialog3(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.bluelightarticle);
+
+        ImageView closedialog = dialog.findViewById(R.id.Closedialogbtn);
+        closedialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {dialog.dismiss();}
+        });
+
+
+        dialog.show();
+    }
+
 
     private void Showdialog2() {
         Dialog dialog = new Dialog(this);
